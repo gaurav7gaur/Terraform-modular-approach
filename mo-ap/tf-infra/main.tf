b62aafb7ae2535a0a27a069d07e7f804b68b53e1
Gaurav Gaur <gaurav7gaur@gmail.com>
2025-02-04 15:32:15 +0530

    added new subnet and region

--- mo-ap/tf-infra/main.tf
+++ mo-ap/tf-infra/main.tf
@@ -1,17 +1,18 @@
 terraform {
-  /*backend "azurerm" {
-    resource_group_name  = "tf-backend"
-    storage_account_name = "tfstatefilebackend01"
-    container_name       = "tf-testing-infra"
-    key                  = "3-tier-infra.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "backend-rg"
+    storage_account_name = "tfbackendgglearn"
+    container_name       = "tf-backend"
+    key                  = "backend.tfstate"
 
-  }*/
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.16.0"
     }
   }
+
 }
 
 provider "azurerm" {
@@ -21,7 +22,16 @@ provider "azurerm" {
       skip_shutdown_and_force_delete = true
     }*/
   }
-  subscription_id = "4d14920f-f57e-4470-8ba0-04827bfd7f03"
+
+  subscription_id = "7a200d71-c12e-4edd-a634-3bb2ff9c7144"
+  /*
+  //client_id = "de20e3cd-6d07-42c5-84df-84ae74cdc7ab"
+  client_id = "ae721cbb-49a9-41ff-a460-212db29c5718"
+
+  client_secret = "FII8Q~dhhYauks_Np1z2MI95nVOOJT79PkW4Sa~K"
+
+  tenant_id = "a6859188-9a5a-4be5-a3dc-4072291d2fe1"
+*/
 }
 
 variable "type" {
@@ -33,14 +43,20 @@ variable "type" {
 
 module "RG" {
   source   = "./RG"
-  rg_name  = "${var.type}-rg"
+  rg_name  = "${var.type}-rg01"
   location = "centralindia"
 }
 
+module "rg2" {
+  source   = "./RG"
+  rg_name  = "${var.type}-rg02"
+  location = "southindia"
+
+}
 #commenting all below to destroy the resources
 
 
-module "VNET" {
+module "VNET1" {
   source        = "./VNet"
   rg_name       = module.RG.rg_name
   vnet_name     = "${var.type}-vnet"
@@ -48,13 +64,29 @@ module "VNET" {
   location      = module.RG.location
 }
 
+module "Vnet2" {
+  source        = "./VNet"
+  rg_name       = module.rg2.rg_name
+  vnet_name     = "${var.type}-vnet2"
+  address_space = ["21.0.0.0/20"]
+}
+
 module "subnet1" {
   source               = "./subnet"
   subnet_name          = "subnet1"
-  rg_name              = module.VNET.vnet_rg
-  location             = module.VNET.vnet_location
+  rg_name              = module.VNET1.vnet_rg
+  location             = module.VNET1.vnet_location
   subnet_address_space = ["22.0.0.0/24"]
-  vnet_name            = module.VNET.vnet_name
+  vnet_name            = module.VNET1.vnet_name
+}
+
+module "subnet2-1" {
+  source               = "./subnet"
+  subnet_name          = "subnet1"
+  rg_name              = module.Vnet2.vnet_rg
+  location             = module.Vnet2.vnet_location
+  subnet_address_space = ["21.0.0.0/24"]
+  vnet_name            = module.Vnet2.vnet_name
 }
 
 module "NSG1" {
